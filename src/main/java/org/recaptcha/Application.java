14e79c5fdfba6bfd818a113b5c0f66726d4450f1
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-03-01 10:39:46 +0500

    added auto starter

--- src/main/java/org/recaptcha/Application.java
+++ src/main/java/org/recaptcha/Application.java
@@ -1,7 +1,15 @@
 package org.recaptcha;
 
+import org.recaptcha.config.AppInit;
+import org.recaptcha.config.Config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication(exclude = AppInit.class)
+@Import(Config.class)
 public class Application {
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class, args);
     }
 }
