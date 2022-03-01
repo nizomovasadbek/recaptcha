a60d1210d704e9dcb0e5f77b6a3e250337f30384
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-03-01 10:37:37 +0500

    fixed some reports

--- src/main/java/org/recaptcha/config/Config.java
+++ src/main/java/org/recaptcha/config/Config.java
@@ -1,10 +1,19 @@
 package org.recaptcha.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-
+@ComponentScan("org.recaptcha")
+@PropertySource("classpath:application.properties")
 public class Config {
+
+    @Bean
+    @Scope("prototype")
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
 }
--- src/main/java/org/recaptcha/controllers/HomeController.java
+++ src/main/java/org/recaptcha/controllers/HomeController.java
@@ -1,4 +1,20 @@
 package org.recaptcha.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.http.HttpMethod.POST;
+
 public class HomeController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @GetMapping()
+    public String home(){
+        return "home";
+    }
+
 }
