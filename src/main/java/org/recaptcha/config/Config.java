6b2fee8545cd3346ac63eaac36af6a856e0b4e29
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-03-01 10:29:27 +0500

    Added auto import

--- src/main/java/org/recaptcha/config/Config.java
+++ src/main/java/org/recaptcha/config/Config.java
@@ -1,5 +1,10 @@
 package org.recaptcha.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
 
 public class Config {
 }
