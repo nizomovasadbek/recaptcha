0525fd2b5d9c48daa71049157b248284b86bef56
nizomovasadbek <nizomovasadbekjava@gmail.com>
2022-03-01 11:57:07 +0500

    Big chance to ignore

--- src/main/java/org/recaptcha/controllers/HomeController.java
+++ src/main/java/org/recaptcha/controllers/HomeController.java
@@ -1,11 +1,13 @@
 package org.recaptcha.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
 @Controller
+@PropertySource("classpath:application.properties")
 public class HomeController {
 
     @Autowired
@@ -14,6 +16,11 @@ public class HomeController {
     @GetMapping
     public String homePage(){
         return "home";
+    }
+
+    @GetMapping("/login")
+    public String loginPage(){
+        return "login";
     }
 
 }
